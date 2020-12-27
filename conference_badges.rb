55b002a220142ec0154deac89e2e0f7d714b330b
Shaina C <scailles23@gmail.com>
2020-12-27 09:07:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+
+def badge_maker(name="Arel")
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+attendees.collect do |name|
+  badge_maker(name)
+end
+end
+
+
+def assign_rooms(attendees)
+  room_ass = []
+  room = 1
+  until room == 7 do 
+attendees.each do |name, index|
+  room_ass << "Hello #{name}! You'll be assigned to room #{room}!"
+end
+end
+room += 1
+room_ass
+end
