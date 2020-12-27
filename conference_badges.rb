a39b44f5e0f7aabdd3d0ec0a7084517607527a3c
Shaina C <scailles23@gmail.com>
2020-12-27 09:09:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,6 @@ end
 def assign_rooms(attendees)
   room_ass = []
   room = 1
-  until room == 7 do 
 attendees.each do |name, index|
   room_ass << "Hello #{name}! You'll be assigned to room #{room}!"
 end
